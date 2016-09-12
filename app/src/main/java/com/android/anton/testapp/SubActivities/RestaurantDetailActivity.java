9e32cdd4bcfdc0a944db90d59eab1f3216311135
Mobile719 <anton.shir719@gmail.com>
2016-09-12 16:53:37 +0300

    Changed Tabbar

---
+++ app/src/main/java/com/android/anton/testapp/SubActivities/RestaurantDetailActivity.java
@@ -0,0 +1,15 @@
+package com.android.anton.testapp.SubActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.anton.testapp.R;
+
+public class RestaurantDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
